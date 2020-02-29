31b98a6910a196f7c176538ab797ed77074e5ac2
Robert Novotny <novotnr0@gmail.com>
2020-02-29 11:05:47 +0100

    Pridana master aktivita so zoznamom RecyclerView

---
+++ app/src/main/java/com/github/novotnyr/android/flags/FlagListActivity.java
@@ -0,0 +1,20 @@
+package com.github.novotnyr.android.flags;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class FlagListActivity extends AppCompatActivity {
+    private RecyclerView flagsRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flag_list);
+
+        flagsRecyclerView = findViewById(R.id.flagsRecyclerView);
+        flagsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
