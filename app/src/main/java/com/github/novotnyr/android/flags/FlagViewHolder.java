a595370ab510adb8343a70aa0302492d3281a0d9
Robert Novotny <novotnr0@gmail.com>
2020-02-29 11:07:14 +0100

    Pridany viewholder pre polozku v zozname

---
+++ app/src/main/java/com/github/novotnyr/android/flags/FlagViewHolder.java
@@ -0,0 +1,19 @@
+package com.github.novotnyr.android.flags;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FlagViewHolder extends RecyclerView.ViewHolder {
+    private TextView textView;
+
+    public FlagViewHolder(View layout) {
+        super(layout);
+        textView = layout.findViewById(android.R.id.text1);
+    }
+
+    public void bind(final Flag flag) {
+        textView.setText(flag.getCountry());
+    }
+}
